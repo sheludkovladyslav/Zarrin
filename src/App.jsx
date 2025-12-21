e9437b5e74bd901553e17d88a552cc9aab0cf4e5
Vladyslav Sheludko <128414119+sheludkovladyslav@users.noreply.github.com>
2025-12-21 13:35:14 -0500

    structure of the project, reset, normalize, fonts, set up. Done header and menu with mobile first technique

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import "./styles/reset.css";
+
+import Nav from "./components/Nav/Nav";
+
+function App() {
+  return (
+    <>
+      <Nav></Nav>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { useRef } from "react";
+import { useState } from "react";
+import { CiSearch } from "react-icons/ci";
+import { TbMenu2 } from "react-icons/tb";
+import navLogo from "./nav__logo.svg";
+import { IoCloseSharp } from "react-icons/io5";
+
+import "./Nav.css";
+
+export default function Nav() {
+  const [isOpened, setOpened] = useState(false);
+
+  const menu = useRef(null);
+
+  const toggleModal = () => {
+    menu.current.classList.toggle("menu__wrapper--active");
+
+    setOpened((prevState) => !prevState);
+  };
+
+  return (
+    <nav className="nav">
+      <div className="nav__container">
+        <div className="nav__logo logo">
+          <div className="logo__container">
+            <img src={navLogo} alt="" className="logo__picture" />
+          </div>
+          <a href="" className="logo__text">
+            Zarrin
+          </a>
+        </div>
+
+        <div className="nav__menu menu">
+          <div className="menu__wrapper" ref={menu}>
+            <ul className="menu__list menu">
+              <li className="menu__item">
+                <a href="">Blog</a>
+              </li>
+              <li className="menu__item">
+                <a href="">About</a>
+              </li>
+              <li className="menu__item menu__item--search">
+                <a href="">
+                  <CiSearch />
+                </a>
+              </li>
+              <li className="menu__item menu__item--contact">
+                <a href="">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+          <div className="menu__open open">
+            <button className="open__btn" onClick={toggleModal}>
+              {isOpened ? <IoCloseSharp /> : <TbMenu2 />}
+            </button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
